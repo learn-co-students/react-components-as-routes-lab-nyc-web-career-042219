c60d2accf843e1bee1823aec3494bec729115d84
Carly Pecora <carly@carlypecora.com>
2019-07-07 21:47:53 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { directors } from '../data';
 
-const Directors = () => {
+class Directors extends React.Component {
+
+	displayDirectors = () => {
+		return directors.map(director => {
+			return(
+				<div>
+				{director.name}
+				{director.movies.map(genre => <ul>{genre}</ul>)}
+				</div>
+			)
+		})
+	}
+
+	render(){
   return (
     <div>
-      {/*{code here}*/}
+    	<h1>Directors Page</h1>
+    	{this.displayDirectors()}
     </div>
   );
+};
 }
-
-export default Directors
+export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
+class Movies extends React.Component {
+
+	displayMovies = () => {
+		return movies.map(movie => {
+			return(
+				<div>
+				{movie.title}
+				{movie.time}
+				{movie.genres.map(genre => <ul>{genre}</ul>)}
+				</div>
+			)
+		})
+	}
+
+	render(){
   return (
     <div>
-        {/*{code here}*/}
+    	<h1>Movies Page</h1>
+    	{this.displayMovies()}
     </div>
   );
 };
-
+}
 export default Movies;
